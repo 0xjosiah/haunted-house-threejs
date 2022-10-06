866a39ce0c695dd7217635fb1f4ec5b05a6059a5
Josiah Webb <jj.webb520@gmail.com>
2022-10-06 14:57:11 -0600

    optimizes shadows

--- src/script.js
+++ src/script.js
@@ -249,20 +249,41 @@ renderer.setClearColor(0x262837)
  * Shadows
  */
 renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
 moonLight.castShadow = true
+
 doorLight.castShadow = true
+doorLight.shadow.mapSize.width = 256
+doorLight.shadow.mapSize.height = 256
+doorLight.shadow.camera.far = 7
+
 ghost1.castShadow = true
+ghost1.shadow.mapSize.width = 256
+ghost1.shadow.mapSize.height = 256
+ghost1.shadow.camera.far = 7
+
 ghost2.castShadow = true
+ghost2.shadow.mapSize.width = 256
+ghost2.shadow.mapSize.height = 256
+ghost2.shadow.camera.far = 7
+
 ghost3.castShadow = true
+ghost3.shadow.mapSize.width = 256
+ghost3.shadow.mapSize.height = 256
+ghost3.shadow.camera.far = 7
 
 walls.castShadow = true
+
 bush1.castShadow = true
 bush2.castShadow = true
 bush3.castShadow = true
 bush4.castShadow = true
 
 floor.receiveShadow = true
+
+
+
 
 /**
  * Animate
